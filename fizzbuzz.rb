c328401600e9126238c4203d85b481bd00e3e30e
George Serna <sernageorge@gmail.com>
2021-02-09 16:43:09 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 def fizzbuzz(number)
   if (number % 3 == 0) && (number % 5 == 0)
-    puts "FizzBuzz"
+    print "FizzBuzz"
   elsif number % 5 == 0
-    puts "Buzz"
+    print "Buzz"
   elsif number % 3 == 0
-    puts "Fizz"
+    print "Fizz"
   end
 end
 
