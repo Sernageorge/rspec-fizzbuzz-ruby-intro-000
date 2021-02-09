fd176422d8f5c1892a1482897843e1c278c76abb
George Serna <sernageorge@gmail.com>
2021-02-09 18:07:08 +0000

    Automatically backed up by Learn

--- bazzfizzbuzz.rb
+++ bazzfizzbuzz.rb
@@ -2,9 +2,9 @@ def fizzbuzz(i.to 100)
   if (i % 3 == 0) (output += puts "Fizz")
   if (i % 5 == 0) (output += puts "Buzz")
   if (i % 7 == 0) (output += puts "Bazz")
-  if (output == " ") 
+  if (output == " ")
 end
 
-output = i 
+output = i
 
 fizzbuzz(70)
