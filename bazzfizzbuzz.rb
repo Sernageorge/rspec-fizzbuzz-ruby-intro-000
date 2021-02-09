929aee8589c9d15c8ab04de239565ea9518846fd
George Serna <sernageorge@gmail.com>
2021-02-09 18:18:14 +0000

    Automatically backed up by Learn

--- bazzfizzbuzz.rb
+++ bazzfizzbuzz.rb
@@ -1,13 +1,6 @@
-1.upto(100) do |i|
-  if i % 5 == 0 and i % 3 == 0
-    puts "FizzBuzz"
-  elsif i % 5 == 0
-    puts "Buzz"
-  elsif i % 3 == 0
-    puts "Fizz"
-  else
-    puts i
-  end
-end
-
-i 
+(1..100).each{|i|
+  x = ' '
+  x += 'Fizz' if i%3==0
+  x += 'Fizz' if i%5==0
+  puts (x.empty? ? i : x);
+}
